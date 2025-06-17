80525d27c5927449b75a1b142fe8ccb955d795ce
Anjum Jahan <150241308+Student-AnjumJahan@users.noreply.github.com>
2025-06-17 09:49:10 +0530

    server.js updated for vercel

--- server/server.js
+++ server/server.js
@@ -55,12 +55,16 @@ app.use("/api/messages", messageRouter);
 await connectDB();
 
 
-const PORT = process.env.PORT || 5000;
+if(process.env.NODE_ENV !== "production"){
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => {
+        console.log(`Server is running on PORT: http://localhost:${PORT}`)
+    })
+}
 
-server.listen(PORT, () => {
-    console.log(`Server is running on PORT: http://localhost:${PORT}`)
-})
+// export server for vercel
+export default server;
 
 
 
-// mongodb+srv://anjum_jahan:Anjum%402002@cluster0.syqq6u3.mongodb.net
+
